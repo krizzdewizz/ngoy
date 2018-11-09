178bd4799686522c25b5e72d17d336fc742e097a
Christian Oetterli <christian.oetterli@bison-group.com>
2018-11-09 01:51:58 +0100

    add test-web

---
+++ test-web/src/main/java/org/ngoy/TestWebApplication.java
@@ -0,0 +1,12 @@
+package org.ngoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestWebApplication.class, args);
+	}
+}
---
+++ test-web/src/main/java/org/ngoy/ViewController.java
@@ -0,0 +1,52 @@
+package org.ngoy;
+
+import static org.ngoy.core.Provider.useValue;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.ngoy.core.Context;
+import org.ngoy.core.TemplateCache;
+import org.ngoy.testapp.PersonService;
+import org.ngoy.testapp.TestApp;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ViewController {
+
+	private static Ngoy ngoy;
+
+	@GetMapping(path = "/")
+	@ResponseBody
+	public void greeting(@RequestParam(name = "name", required = false, defaultValue = "world") String name, HttpServletResponse response) throws Exception {
+
+		// simply render a template, bind to variables. No 'app' needed
+
+		Context ctx = Context.of()
+				.variable("name", name);
+		Ngoy.render("/templates/greeting.html", ctx, response.getOutputStream());
+	}
+
+	@GetMapping(path = "/p")
+	@ResponseBody
+	public void persons(HttpServletResponse response) throws Exception {
+		TemplateCache.DEFAULT.setDisabled(true);
+		app().render(response.getOutputStream());
+	}
+
+	private Ngoy app() {
+		if (ngoy == null) {
+			PersonService personService = new PersonService();
+			ngoy = Ngoy.app(TestApp.class)
+					.providers(useValue(PersonService.class, personService))
+					.locale(Locale.GERMAN)
+					.translateBundle("messages")
+					.build();
+		}
+		return ngoy;
+	}
+}
