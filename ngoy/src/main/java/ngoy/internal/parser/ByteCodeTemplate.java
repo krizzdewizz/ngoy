f9e05cd79ae5afebce16391b6c23f0de42797541
Christian Oetterli <christian.oetterli@bison-group.com>
2018-11-25 22:41:41 +0100

    cleanup

--- ngoy/src/main/java/ngoy/internal/parser/ByteCodeTemplate.java
+++ ngoy/src/main/java/ngoy/internal/parser/ByteCodeTemplate.java
@@ -31,7 +31,7 @@ public class ByteCodeTemplate implements ParserHandler {
 		@Override
 		protected void doPrint(String text, boolean isExpr) {
 			if (isExpr) {
-				throw new NgoyException("Must not be called for expressions. Please contact developer.");
+				throw new NgoyException("Must not be called for expressions.");
 			}
 
 			printOut(text);
--- ngoy/src/main/java/ngoy/internal/parser/visitor/NodeVisitor.java
+++
@@ -1,1 +0,0 @@
-package ngoy.internal.parser.visitor;
