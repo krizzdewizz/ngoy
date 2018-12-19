ca540f01529c5ea0f907331fc68bda8bc1ca3498
Christian Oetterli <christian.oetterli@bison-group.com>
2018-12-19 20:27:14 +0100

    add api to use type of object for model and variable

--- ngoy/src/main/java/ngoy/core/Context.java
+++ ngoy/src/main/java/ngoy/core/Context.java
@@ -1,5 +1,7 @@
 package ngoy.core;
 
+import static java.util.Objects.requireNonNull;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,22 +25,54 @@ public final class Context<T> {
 
 	/**
 	 * Returns a context for the given model.
+	 * <p>
+	 * Uses {@link Object#getClass()} for the model class.
 	 * 
 	 * @param model Model
 	 * @return Context
 	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static <T> Context<T> of(T model) {
+		requireNonNull(model, "Model must not be null");
+		return new Context(model.getClass(), model, new HashMap<>());
+	}
+
+	/**
+	 * Returns a context for the given model.
+	 * 
+	 * @param modelClass Class of the model
+	 * @param model      Model
+	 * @return Context
+	 */
 	public static <T> Context<T> of(Class<T> modelClass, T model) {
-		return new Context<T>(modelClass, model, new HashMap<>());
+		return new Context<T>(modelClass, requireNonNull(model, "Model must not be null"), new HashMap<>());
+	}
+
+	/**
+	 * Returns a context initialized with the given variable.
+	 * <p>
+	 * Uses {@link Object#getClass()} for the variable type.
+	 * 
+	 * @param variableName  Name of the variable
+	 * @param variableValue Value of the variable
+	 * @return Context
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static Context<?> of(String variableName, Object variableValue) {
+		requireNonNull(variableValue, "Variable value must not be null");
+		Context ctx = Context.of();
+		return ctx.variable(variableName, variableValue.getClass(), variableValue);
 	}
 
 	/**
 	 * Returns a context initialized with the given variable.
 	 * 
 	 * @param variableName  Name of the variable
+	 * @param variableType  Type of the variable
 	 * @param variableValue Value of the variable
 	 * @return Context
 	 */
-	public static <V> Context<?> of(String variableName, Class<V> variableType, V variableValue) {
+	public static <V> Context<?> of(String variableName, Class<V> variableType, @Nullable V variableValue) {
 		return Context.of()
 				.variable(variableName, variableType, variableValue);
 	}
@@ -55,12 +89,29 @@ public final class Context<T> {
 
 	/**
 	 * Adds a variable.
+	 * <p>
+	 * Uses {@link Object#getClass()} for the variable type.
 	 * 
-	 * @param variableName
+	 * @param variableName  Name of the variable
+	 * @param variableValue must not be null
+	 * @return this
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public Context<T> variable(String variableName, Object variableValue) {
+		requireNonNull(variableValue, "Variable value must not be null");
+		variables.put(variableName, new Variable(variableValue.getClass(), variableValue));
+		return this;
+	}
+
+	/**
+	 * Adds a variable.
+	 * 
+	 * @param variableName  Name of the variable
+	 * @param variableType  Type of the variable
 	 * @param variableValue may be null
 	 * @return this
 	 */
-	public <V> Context<T> variable(String variableName, Class<V> variableType, V variableValue) {
+	public <V> Context<T> variable(String variableName, Class<V> variableType, @Nullable V variableValue) {
 		variables.put(variableName, new Variable<V>(variableType, variableValue));
 		return this;
 	}
--- ngoy/src/test/java/ngoy/core/ContextTest.java
+++ ngoy/src/test/java/ngoy/core/ContextTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
 
+import ngoy.model.Person;
+
 public class ContextTest {
 
 	@Test
@@ -17,9 +19,31 @@ public class ContextTest {
 	}
 
 	@Test
+	public void testOfRoot2() {
+		Context<Person> context = Context.of(new Person("Peter", 22));
+		assertThat(context).isNotNull();
+		assertThat(context.getModelClass()).isEqualTo(Person.class);
+	}
+
+	@Test
 	public void testVariable() {
 		Context<?> of = Context.of();
 		Context<?> context = of.variable("a", String.class, "1");
 		assertThat(context).isSameAs(of);
+	}
+
+	@Test
+	public void testVariableDeduceClass() {
+		Context<?> context = Context.of()
+				.variable("a", "1");
+		assertThat(context.getVariables()
+				.get("a").type).isEqualTo(String.class);
+	}
+
+	@Test
+	public void testVariableDeduceClass2() {
+		Context<?> context = Context.of("a", "1");
+		assertThat(context.getVariables()
+				.get("a").type).isEqualTo(String.class);
 	}
 }
