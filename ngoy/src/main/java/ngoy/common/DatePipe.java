cc496662a0ba74000b8e55dd6c2ba8577fbc2fcd
Christian Oetterli <christian.oetterli@bison-group.com>
2018-12-21 09:13:42 +0100

    DatePipe supports different types

--- ngoy/src/main/java/ngoy/common/DatePipe.java
+++ ngoy/src/main/java/ngoy/common/DatePipe.java
@@ -1,11 +1,19 @@
 package ngoy.common;
 
+import static java.lang.String.format;
+import static java.util.Arrays.asList;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 import ngoy.core.Inject;
 import ngoy.core.LocaleProvider;
+import ngoy.core.NgoyException;
 import ngoy.core.Nullable;
+import ngoy.core.Optional;
 import ngoy.core.Pipe;
 import ngoy.core.PipeTransform;
 
@@ -32,31 +40,50 @@ import ngoy.core.PipeTransform;
 @Pipe("date")
 public class DatePipe implements PipeTransform {
 
-	private static final String DEFAULT_PATTERN = "dd.MM.yyyy hh:mm:ss";
+	public static class Config {
+		public String defaultDatePattern = "dd.MM.yyyy HH:mm:ss";
+		public String defaultLocalDatePattern = "dd.MM.yyyy";
+		public String defaultLocalDateTimePattern = format("%s hh:mm:ss", defaultLocalDatePattern);
+	}
+
+	private static final Config DEFAULT_CONFIG = new Config();
 
 	@Inject
 	public LocaleProvider localeProvider;
+
+	@Inject
+	@Optional
+	public Config config = DEFAULT_CONFIG;
 
 	@Override
 	public Object transform(@Nullable Object obj, Object... params) {
 		if (obj == null) {
 			return null;
 		}
-		String pattern = params.length == 0 ? DEFAULT_PATTERN : params[0].toString();
-		LocalDateTime localDate = (LocalDateTime) obj;
-		return format(localDate, pattern);
+
+		if (obj instanceof LocalDateTime) {
+			return formatter(formatPattern(params, config.defaultLocalDateTimePattern)).format((LocalDateTime) obj);
+		} else if (obj instanceof LocalDate) {
+			return formatter(formatPattern(params, config.defaultLocalDatePattern)).format((LocalDate) obj);
+		} else if (obj instanceof Date) {
+			return formatDate((Date) obj, params, config.defaultDatePattern);
+		} else if (obj instanceof Long) {
+			return formatDate(new Date((long) obj), params, config.defaultDatePattern);
+		} else {
+			throw new NgoyException("DatePipe: input must be one of type %s", asList(LocalDate.class.getName(), LocalDateTime.class.getName(), Date.class.getName(), long.class.getName()));
+		}
 	}
 
-	/**
-	 * Formats the given date with the given pattern using the current locale.
-	 * 
-	 * @param date
-	 * @param pattern
-	 * @return Formatted date
-	 * @see DateTimeFormatter
-	 */
-	public String format(LocalDateTime date, String pattern) {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern, localeProvider.getLocale());
-		return date.format(formatter);
+	private String formatDate(Date date, Object[] params, String defPattern) {
+		String pattern = formatPattern(params, defPattern);
+		return new SimpleDateFormat(pattern, localeProvider.getLocale()).format(date);
+	}
+
+	private DateTimeFormatter formatter(String pattern) {
+		return DateTimeFormatter.ofPattern(pattern, localeProvider.getLocale());
+	}
+
+	private String formatPattern(Object[] params, String def) {
+		return params.length == 0 ? def : params[0].toString();
 	}
 }
--- ngoy/src/test/java/ngoy/common/DatePipeTest.java
+++ ngoy/src/test/java/ngoy/common/DatePipeTest.java
@@ -2,14 +2,22 @@ package ngoy.common;
 
 import static ngoy.core.Provider.useValue;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Locale;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import ngoy.ANgoyTest;
 import ngoy.core.Component;
 import ngoy.core.LocaleProvider;
+import ngoy.core.NgoyException;
 
 public class DatePipeTest extends ANgoyTest {
 
@@ -46,6 +54,21 @@ public class DatePipeTest extends ANgoyTest {
 
 	//
 
+	@Component(selector = "", template = "{{ localDate | date }}, {{ localDateTime | date }}, {{ date | date }}, {{ dateLong | date }}")
+	public static class AllDatesCmp {
+		public LocalDate localDate = java.time.LocalDate.of(2018, 10, 28);
+		public LocalDateTime localDateTime = java.time.LocalDateTime.of(2018, 10, 28, 12, 44);
+		public Date date = new GregorianCalendar(2018, 10, 28, 12, 44).getTime();
+		public long dateLong = date.getTime();
+	}
+
+	@Test
+	public void testAllDates() {
+		assertThat(render(AllDatesCmp.class)).isEqualTo("28.10.2018, 28.10.2018 12:44:00, 28.11.2018 12:44:00, 28.11.2018 12:44:00");
+	}
+
+	//
+
 	@Component(selector = "", template = "{{ null | date }}")
 	public static class NullCmp {
 	}
@@ -54,4 +77,36 @@ public class DatePipeTest extends ANgoyTest {
 	public void testNull() {
 		assertThat(render(NullCmp.class)).isEqualTo("");
 	}
+
+	//
+
+	@Component(selector = "", template = "{{ localDate | date }}")
+	public static class WithConfigCmp {
+		public LocalDate localDate = java.time.LocalDate.of(2018, 10, 28);
+	}
+
+	@Test
+	public void testWithConfig() {
+		DatePipe.Config config = new DatePipe.Config();
+		config.defaultLocalDatePattern = "dd. MMMM yyyy";
+		assertThat(render(WithConfigCmp.class, builder -> builder.providers(useValue(DatePipe.Config.class, config)))).isEqualTo("28. October 2018");
+	}
+
+	//
+
+	@Rule
+	public ExpectedException expectedEx = ExpectedException.none();
+
+	@Component(selector = "", template = "{{ dateString | date }}")
+	public static class WrongTypeCmp {
+		public String dateString = "1.1.2018";
+	}
+
+	@Test
+	public void testWrongType() {
+		expectedEx.expect(NgoyException.class);
+		expectedEx.expectMessage(containsString("DatePipe: input must be one of"));
+		render(WrongTypeCmp.class);
+	}
+
 }
